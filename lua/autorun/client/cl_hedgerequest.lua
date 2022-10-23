c2cb7651dc362a4d71f3eba7dfc9905c3d9eb449
Hedges <84698243+hedges7@users.noreply.github.com>
2022-10-23 05:06:06 -0400

    Co-authored-by: Pdzly <34943569+Pdzly@users.noreply.github.com>

--- lua/autorun/client/cl_hedgerequest.lua
+++ lua/autorun/client/cl_hedgerequest.lua
@@ -1,15 +1,12 @@
 AddCSLuaFile("autorun/sh_hedgetransfer_config.lua")
 
-net.Receive("hta_receiving_request", function (len, ply)
-
+net.Receive("hta_receiving_request", function(len, ply)
     -- Receive amount player requested
     amountRequested = net.ReadUInt(32)
+    requestId = net.ReadString()
     print(amountRequested)
-
     -- Receive player who sent request
     sendingPlayer = net.ReadEntity()
     print(sendingPlayer)
-
-    chat.AddText("\n"..sendingPlayer:Nick().." Has Requested "..amountRequested.."$,\nIf you wish to deny the request just ignore it!\nTo accept type /acceptrequest (name)\n")
-
+    chat.AddText("\n" .. sendingPlayer:Nick() .. " Has Requested " .. amountRequested .. "$,\nTo deny it type \"/declinerequest " .. requestId .. "\"\nTo accept type \"/acceptrequest " .. requestId .. "\"")
 end)
